96a518882d56fc250dd9d23209f632ff7d760ede
Mayuri <63649939+MayuriNFC@users.noreply.github.com>
2021-12-31 17:31:26 +0800

    Update gocq.sh

--- gocq.sh
+++ gocq.sh
@@ -1,4 +1,4 @@
 cd /home/`whoami`/qqbot
-rm  -rf /home/root/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}
+rm  -f /home/root/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}/*
 ./go-cqhttp faststart
 sh /home/`whoami`/qqbot/gocq.sh
