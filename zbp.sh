0c6a1d08fb19449bfe31d1ef94c6730286b1d6c2
Mayuri <63649939+myrnfc@users.noreply.github.com>
2022-04-10 03:01:19 +0800

    Update zbp.sh

--- zbp.sh
+++ zbp.sh
@@ -1,4 +1,4 @@
 cd /home/`whoami`/qqbot/
 rm  -rf /home/root/qqbot/data/{leveldb-v2,Reborn,SetuTime,videos,images,VtbQuotation,sleep,hs,voices,cache,acgimage,Funny}
-./zbp -d -p "//"
+./zbp -d -p ">" 1770747317 3501560157 320279493 1847521416 2227300166 1456804473 3416885985 7047694 1343396474 213864964 2053643949 1192200850 1011274397 1184861155 2424391365
 sh /home/`whoami`/qqbot/zbp.sh
