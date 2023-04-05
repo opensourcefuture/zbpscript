9ea6df86935d48ac87c28648f55720bd56a81c4b
DataEraserC <102341238+DataEraserC@users.noreply.github.com>
2023-04-05 14:24:01 +0800

    Update qqbot.sh

--- qqbot.sh
+++ qqbot.sh
@@ -1,5 +1,7 @@
 echo "This_scrpit_will_install_zbp_to_ ~/qqbot/"
-mkdir -p ~/qqbot/{logs,go-cqhttp,zbp}
+mkdir -p ~/qqbot/logs
+mkdir -p ~/qqbot/go-cqhttp
+mkdir -p ~/qqbot/zbp
 wget -O ~/qqbot/gocq.sh https://raw.githubusercontent.com/opensourcefuture/zbpscript/main/gocq.sh
 wget -O ~/qqbot/zbp.sh https://raw.githubusercontent.com/opensourcefuture/zbpscript/main/zbp.sh
 wget -O ~/qqbot/update.sh https://raw.githubusercontent.com/opensourcefuture/zbpscript/main/update.sh 
