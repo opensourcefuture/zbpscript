b6c640caef29b5463e118f57518e64bae1eb299c
雑魚~雑魚~ <102341238+DataEraserC@users.noreply.github.com>
2023-07-01 22:02:11 +0800

    Update gocq.sh

--- gocq.sh
+++ gocq.sh
@@ -1,5 +1,7 @@
 cd ~/qqbot/go-cqhttp
 #rm  -rf ~/qqbot/data/{leveldb-v2,Reborn,videos,images,voices,cache,Funny}
+TZ='Asia/Shanghai'; export TZ
+LC_ALL="zh_CN.utf-8"; export TZ
 chmod +x go-cqhttp
 ./go-cqhttp faststart
 sh ~/qqbot/gocq.sh
