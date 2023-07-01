5c64171d7970e5015d5aa8a4d2d580fa5228d844
雑魚~雑魚~ <102341238+DataEraserC@users.noreply.github.com>
2023-07-01 22:02:25 +0800

    Update zbp.sh

--- zbp.sh
+++ zbp.sh
@@ -1,5 +1,7 @@
 cd ~/qqbot/zbp
 #rm  -rf ~/qqbot/data/{leveldb-v2,Reborn,videos,images,voices,cache,Funny}
+TZ='Asia/Shanghai'; export TZ
+LC_ALL="zh_CN.utf-8"; export TZ
 chmod +x zbp
 ./zbp -c config.json
 sh ~/qqbot/zbp.sh
